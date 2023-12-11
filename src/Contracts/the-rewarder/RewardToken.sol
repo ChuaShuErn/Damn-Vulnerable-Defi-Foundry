e84841c824707efd38a67e4db9dadbe8d1bcacf6
Chua, Shu Ern <shuernchua@kapps.com.sg>
2023-12-11 21:09:10 +0800

    Update RewardToken.sol

--- src/Contracts/the-rewarder/RewardToken.sol
+++ src/Contracts/the-rewarder/RewardToken.sol
@@ -24,3 +24,57 @@ contract RewardToken is ERC20, AccessControl {
         _mint(to, amount);
     }
 }
+
+//    /**
+//      * @dev Returns `true` if `account` has been granted `role`.
+//      */
+//     function hasRole(bytes32 role, address account) public view virtual override returns (bool) {
+//         return _roles[role].members[account];
+//     }
+
+// /**
+//      * @dev Grants `role` to `account`.
+//      *
+//      * If `account` had not been already granted `role`, emits a {RoleGranted}
+//      * event. Note that unlike {grantRole}, this function doesn't perform any
+//      * checks on the calling account.
+//      *
+//      * May emit a {RoleGranted} event.
+//      *
+//      * [WARNING]
+//      * ====
+//      * This function should only be called from the constructor when setting
+//      * up the initial roles for the system.
+//      *
+//      * Using this function in any other way is effectively circumventing the admin
+//      * system imposed by {AccessControl}.
+//      * ====
+//      *
+//      * NOTE: This function is deprecated in favor of {_grantRole}.
+//      */
+//     function _setupRole(bytes32 role, address account) internal virtual {
+//         _grantRole(role, account);
+//     }
+
+// /**
+//      * @dev Grants `role` to `account`.
+//      *
+//      * Internal function without access restriction.
+//      *
+//      * May emit a {RoleGranted} event.
+//      */
+//     function _grantRole(bytes32 role, address account) internal virtual {
+//         if (!hasRole(role, account)) {
+//             _roles[role].members[account] = true;
+//             emit RoleGranted(role, account, _msgSender());
+//         }
+//     }
+
+//  struct RoleData {
+//         mapping(address => bool) members;
+//         bytes32 adminRole;
+//     }
+
+//     mapping(bytes32 => RoleData) private _roles;
+
+//     bytes32 public constant DEFAULT_ADMIN_ROLE = 0x00;
